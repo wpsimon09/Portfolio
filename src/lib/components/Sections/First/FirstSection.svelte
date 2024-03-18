18a2c3e75043a6403323176ff36c200de393a432
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-18 15:14:16 +0100

    added scroll down animation

--- src/lib/components/Sections/First/FirstSection.svelte
+++ src/lib/components/Sections/First/FirstSection.svelte
@@ -2,17 +2,26 @@
     import Information from "./SectionComponents/Information.svelte";
     import ThreeJs from "./SectionComponents/ThreeJS.svelte";
     import ThreeJS from "./SectionComponents/ThreeJS.svelte";
-    
+    import TopBar from "./SectionComponents/TopBar.svelte";
+    import ScrollDownIcon from "./SectionComponents/ScrollDownIcon.svelte";
 </script>
 
 <section class="w-full h-full relative dark:bg-slate-950 bg-slate-100">
     <div class="flex flex-col items-center w-full h-full ">
+        <div class="w-full z-20">
+            <TopBar/>
+        </div>
         <div class="z-10 flex flex-row h-full items-center relative">
             <Information/>
         </div>
     </div>  
-    <div class="w-full h-full  absolute inset-0">
-        <ThreeJs />
+    <div class="w-full h-full flex-col items-center absolute inset-0">
+        <div class=" relative">   
+            <ThreeJs />
+            <div class="w-full  absolute ">
+                <ScrollDownIcon/>
+            </div>
+        </div>
     </div>
 </section>
 
---
+++ src/lib/components/Sections/First/SectionComponents/ScrollDownIcon.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<div class="w-full flex flex-col items-center">
+    <div>
+        <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+
+        <dotlottie-player src="https://lottie.host/813c7ccc-c4de-47ab-9cdf-f818cb5750aa/NSdQjcQlwW.json" background="transparent" speed="1" style="width: 80px; height: 80px;" loop autoplay></dotlottie-player>
+    </div>
+</div>
