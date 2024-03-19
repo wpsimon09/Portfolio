8297ae5bc45b08e4a7ec40fdcc95c89d42688fad
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-19 17:26:09 +0100

    made different parts of about me page

--- src/lib/components/Sections/Second/Components/Header.svelte
+++ src/lib/components/Sections/Second/Components/Header.svelte
@@ -1,17 +1,25 @@
 <script>
     export let headerText;
-    export let isHoveredOn ;
+    export let isHoveredOn;
 
     let textColor;
+    
     if (headerText === "About me") {
-        textColor = "slate-950";
+        textColor = "slate-900";
     } else {
         textColor = "slate-100";
     }
+    
+    let centerLocation = isHoveredOn ? "" : "justify-center"
+
 </script>
 
-<div class="w-full h-full flex flex-col items-center justify-center">
-    <h1 class="text-6xl lg:text-9xl text-{textColor} font-capriola">
+<div class="w-full h-full flex flex-col items-center {centerLocation} ">
+    <h1 class="text-6xl lg:text-9xl text-{textColor} font-capriola ">
         {headerText}
     </h1>
+    {#if isHoveredOn}
+        <p>is between 5 and 10</p>
+    {:else}
+    {/if}
 </div>
