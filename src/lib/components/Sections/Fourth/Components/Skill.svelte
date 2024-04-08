9483e6250235099204886a4ec70a855583f84687
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-08 20:30:02 +0200

    created random margin of circles

--- src/lib/components/Sections/Fourth/Components/Skill.svelte
+++ src/lib/components/Sections/Fourth/Components/Skill.svelte
@@ -1,10 +1,19 @@
 <script>
-    export let offset;
-    export let scale;
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
 
+	let offset;
+	export let index;
+	if (browser) {
+		function calculateEffect(i) {
+			let result = Math.round(Math.random() * i * 2) * 50;
+			result = 'margin-top:' + result + 'px';
+			return result;
+		}
 
+		offset = calculateEffect(index);
+		console.log(offset);
+	}
 </script>
 
-<div class="rounded-full w-40 h-40 bg-red-200">
-    
-</div>
+<div class="rounded-full w-40 h-40 bg-red-200" style="{offset}"></div>
--- src/lib/components/Sections/Fourth/Fourth.svelte
+++ src/lib/components/Sections/Fourth/Fourth.svelte
@@ -1,17 +1,18 @@
 <script>
-    import skills from "./skills.json"
-    import Skill from "./Components/Skill.svelte"
+	import skills from './skills.json';
+	import Skill from './Components/Skill.svelte';
+	
 </script>
 
-<section class="h-screen w-screen flex flex-col items-center bg-zinc-100 dark:bg-zinc-800 ">
-	<h1 class="text-6xl lg:text-9xl dark:text-slate-100 text-zinc-800 font-julius  mt-10 ">Skills</h1>
-    <div class="w-full h-full">
-
-        <div class="w-[90%] h-40 gap-4 flex flex-row items-center overflow-x-scroll">
-            {#each skills as skill}
-                <Skill/>
-            {/each}
-        </div>
-        
-    </div>
+<section class="h-screen w-screen flex flex-col items-center bg-zinc-100 dark:bg-zinc-800">
+	<h1 class="text-6xl lg:text-9xl dark:text-slate-100 text-zinc-800 font-julius mt-10">Skills</h1>
+	<div class="w-screen h-full flex flex-col items-center justify-center">
+		<div class="h-80 w-[99%] gap-1 flex flex-row items-center overflow-x-auto">
+			{#each skills as skill, i}
+				<div>
+					<Skill index={i}/>
+				</div>
+			{/each}
+		</div>
+	</div>
 </section>
