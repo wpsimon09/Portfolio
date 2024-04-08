aec8ee54d56eaff0e97a1397e1e71d23d6dc7fb6
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-08 17:45:47 +0200

    first prototype of skills

--- src/lib/components/Sections/Fourth/Fourth.svelte
+++ src/lib/components/Sections/Fourth/Fourth.svelte
@@ -3,12 +3,15 @@
     import Skill from "./Components/Skill.svelte"
 </script>
 
-<section class="h-screen w-screen flex flex-col items-center bg-zinc-100 dark:bg-zinc-800">
+<section class="h-screen w-screen flex flex-col items-center bg-zinc-100 dark:bg-zinc-800 ">
 	<h1 class="text-6xl lg:text-9xl dark:text-slate-100 text-zinc-800 font-julius  mt-10 ">Skills</h1>
-    <div class="w-auto gap-4 flex flex-row items-center overflow-x-scroll">
-        {#each skills as skill}
-            <Skill/>
-        {/each}
+    <div class="w-full h-full">
+
+        <div class="w-[90%] h-40 gap-4 flex flex-row items-center overflow-x-scroll">
+            {#each skills as skill}
+                <Skill/>
+            {/each}
+        </div>
+        
     </div>
-    
 </section>
