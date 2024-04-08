d61f7aa1099bc91797e540bba319b0cac67d64ce
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-08 20:48:39 +0200

    calcualted random scales and margins

--- src/lib/components/Sections/Fourth/Components/Skill.svelte
+++ src/lib/components/Sections/Fourth/Components/Skill.svelte
@@ -4,16 +4,23 @@
 
 	let offset;
 	export let index;
-	if (browser) {
-		function calculateEffect(i) {
-			let result = Math.round(Math.random() * i * 2) * 50;
-			result = 'margin-top:' + result + 'px';
-			return result;
-		}
-
-		offset = calculateEffect(index);
-		console.log(offset);
+	function calculateEffect(i) {
+		let marginFactor = Math.random() * 3 + 1;
+		let margin = Math.round(marginFactor * 10) + 10;
+		let marginType = Math.random() < 0.5 ? 'margin-top' : 'margin-bottom';
+		return `${marginType}:${margin}px`;
 	}
+
+	function calculateScale(i) {
+		let scaleFactor = Math.random() * 0.4 + 0.8;
+		return `transform:scale(${scaleFactor})`;
+	}
+	offset = calculateEffect(index);
+	let scale = calculateScale(index);
+	console.log(offset);
 </script>
 
-<div class="rounded-full w-40 h-40 bg-red-200" style="{offset}"></div>
+<div
+	class="rounded-full w-40 h-40 bg-red-200 hover:w-60 hover:h-60 duration-200"
+	style="{offset};{scale}"
+></div>
--- src/lib/components/Sections/Fourth/Fourth.svelte
+++ src/lib/components/Sections/Fourth/Fourth.svelte
@@ -7,7 +7,7 @@
 <section class="h-screen w-screen flex flex-col items-center bg-zinc-100 dark:bg-zinc-800">
 	<h1 class="text-6xl lg:text-9xl dark:text-slate-100 text-zinc-800 font-julius mt-10">Skills</h1>
 	<div class="w-screen h-full flex flex-col items-center justify-center">
-		<div class="h-80 w-[99%] gap-1 flex flex-row items-center overflow-x-auto">
+		<div class="h-80 w-[99%] flex flex-row items-center overflow-x-auto">
 			{#each skills as skill, i}
 				<div>
 					<Skill index={i}/>
